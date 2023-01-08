844d6e6cf6f578445016e8efe29afdf1bed0e344
Sharafadeen01 <sharafadeenz@gmail.com>
2023-01-08 10:50:24 -0800

    5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+
+def no_c(my_string):
+
+        new_string = ""
+
+            for char in my_string:
+
+                        if char not in "cC":
+
+                                        new_string += char
+
+                                            return 
